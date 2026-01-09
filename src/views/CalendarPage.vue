dca9390179498f95d64a0a94cba8907d1c1d9d96
JacquesRaoult <raoult.jacques@gmail.com>
2026-01-09 19:57:25 +0100

    Add date picker to calendar

--- src/views/CalendarPage.vue
+++ src/views/CalendarPage.vue
@@ -1,29 +1,66 @@
 <template>
-  <ion-page>
-    <ion-header>
-      <ion-toolbar color="primary">
-        <ion-buttons slot="start">
-          <ion-menu-button></ion-menu-button>
-        </ion-buttons>
-        <ion-title>Calendrier</ion-title>
-      </ion-toolbar>
-    </ion-header>
-    
-    <ion-content class="ion-padding">
-      <h1>Calendrier</h1>
-      <p>Contenu ici</p>
-    </ion-content>
-  </ion-page>
+    <ion-page>
+        <ion-header>
+            <ion-toolbar color="primary">
+                <ion-buttons slot="start">
+                    <ion-menu-button></ion-menu-button>
+                </ion-buttons>
+                <ion-title>Calendrier</ion-title>
+            </ion-toolbar>
+        </ion-header>
+
+        <ion-content class="ion-padding">
+            <ion-list>
+               
+                <ion-item>
+                    <ion-label>
+                        <h2>{{ selectedDate ? formattedDate : 'Choisir une date' }}</h2>
+                        <p v-if="!selectedDate">Cliquez pour sélectionner</p>
+                    </ion-label>
+                    <ion-datetime-button datetime="datetime" slot="end"></ion-datetime-button>
+                </ion-item>
+            </ion-list>
+
+            <ion-modal :keep-contents-mounted="true" ref="modal">
+                <ion-datetime id="datetime" presentation="date" @ionChange="handleDateChange"></ion-datetime>
+            </ion-modal>
+        </ion-content>
+    </ion-page>
 </template>
 
-<script setup lang="ts">
-import { 
-  IonPage, 
-  IonHeader, 
-  IonToolbar, 
-  IonButtons, 
-  IonMenuButton, 
-  IonTitle, 
-  IonContent 
+<script setup>
+import {
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonButtons,
+    IonMenuButton,
+    IonTitle,
+    IonContent,
+    IonDatetime,
+    IonDatetimeButton,
+    IonModal,
+    IonList,
+    IonItem,
+    IonLabel
 } from '@ionic/vue';
+import { ref, computed } from 'vue';
+
+const modal = ref();
+const selectedDate = ref(null);
+
+const formattedDate = computed(() => {
+    if (!selectedDate.value) return '';
+    const date = new Date(selectedDate.value);
+    return date.toLocaleDateString('fr-FR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+    });
+});
+
+const handleDateChange = (event) => {
+    selectedDate.value = event.detail.value;
+    modal.value.$el.dismiss();
+};
 </script>
