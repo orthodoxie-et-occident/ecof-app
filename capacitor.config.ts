53c05d24e646d458947c34e540e6bcfbd04dba65
JacquesRaoult <raoult.jacques@gmail.com>
2026-03-11 13:14:17 +0100

    Change UI images

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from "@capacitor/cli"
 
 const config: CapacitorConfig = {
   appId: "io.ionic.starter",
-  appName: "ECOF-vue",
+  appName: "ECOF",
   webDir: "dist",
 }
 
