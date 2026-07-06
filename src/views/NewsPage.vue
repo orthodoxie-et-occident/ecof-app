9ec332d629ab62d4ee02ffb01a7ebc9ea6d61c24
JacquesRaoult <raoult.jacques@gmail.com>
2026-07-06 23:57:07 +0200

    v3.4.8 post release

--- src/views/NewsPage.vue
+++ src/views/NewsPage.vue
@@ -5,7 +5,7 @@
         <ion-buttons slot="start">
           <ion-menu-button></ion-menu-button>
         </ion-buttons>
-        <ion-title>Actualités</ion-title>
+        <ion-title>Annonces</ion-title>
       </ion-toolbar>
     </ion-header>
 
